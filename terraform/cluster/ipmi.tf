ec3ab164cb3b884161105d9cb6bfb55ce4105b32
Melvin Hillsman <mhillsma@redhat.com>
2019-07-08 20:04:36 -0500

    Add ipmi port

--- terraform/cluster/ipmi.tf
+++ terraform/cluster/ipmi.tf
@@ -1,8 +1,8 @@
 resource "null_resource" "ipmi_master" {
     provisioner "local-exec" {
         command = <<EOT
-          ipmitool -I lanplus -H ${var.master_ipmi_host} -U ${var.master_ipmi_user} -P ${var.master_ipmi_pass} chassis bootdev pxe;
-          ipmitool -I lanplus -H ${var.master_ipmi_host} -U ${var.master_ipmi_user} -P ${var.master_ipmi_pass} power cycle || ipmitool -I lanplus -H ${var.master_ipmi_host} -U ${var.master_ipmi_user} -P ${var.master_ipmi_pass} power on;
+          ipmitool -I lanplus -H ${var.master_ipmi_host} -p ${var.master_ipmi_port} -U ${var.master_ipmi_user} -P ${var.master_ipmi_pass} chassis bootdev pxe;
+          ipmitool -I lanplus -H ${var.master_ipmi_host} -p ${var.master_ipmi_port} -U ${var.master_ipmi_user} -P ${var.master_ipmi_pass} power cycle || ipmitool -I lanplus -H ${var.master_ipmi_host} -U ${var.master_ipmi_user} -P ${var.master_ipmi_pass} power on;
 EOT
     }
 }
@@ -11,7 +11,7 @@ resource "null_resource" "ipmi_master_cleanup" {
     provisioner "local-exec" {
         when = "destroy"
         command = <<EOT
-          ipmitool -I lanplus -H ${var.master_ipmi_host} -U ${var.master_ipmi_user} -P ${var.master_ipmi_pass} power off;
+          ipmitool -I lanplus -H ${var.master_ipmi_host} -p ${var.master_ipmi_port} -U ${var.master_ipmi_user} -P ${var.master_ipmi_pass} power off;
 EOT
     }
 }
