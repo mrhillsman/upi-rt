a9e66cfee37ae60ea0e75fd174984e3d816215b1
Melvin Hillsman <mhillsma@redhat.com>
2019-07-09 09:51:58 -0500

    Change cycle to reset for libvirt

--- terraform/workers/ipmi.tf
+++ terraform/workers/ipmi.tf
@@ -2,7 +2,7 @@ resource "null_resource" "ipmi_worker" {
     provisioner "local-exec" {
         command = <<EOT
           ipmitool -I lanplus -H ${var.worker_ipmi_host} -p ${var.worker_ipmi_port} -U ${var.worker_ipmi_user} -P ${var.worker_ipmi_pass} chassis bootdev pxe;
-          ipmitool -I lanplus -H ${var.worker_ipmi_host} -p ${var.worker_ipmi_port} -U ${var.worker_ipmi_user} -P ${var.worker_ipmi_pass} power cycle || ipmitool -I lanplus -H ${var.worker_ipmi_host} -p ${var.worker_ipmi_port} -U ${var.worker_ipmi_user} -P ${var.worker_ipmi_pass} power on;
+          ipmitool -I lanplus -H ${var.worker_ipmi_host} -p ${var.worker_ipmi_port} -U ${var.worker_ipmi_user} -P ${var.worker_ipmi_pass} power reset || ipmitool -I lanplus -H ${var.worker_ipmi_host} -p ${var.worker_ipmi_port} -U ${var.worker_ipmi_user} -P ${var.worker_ipmi_pass} power on;
 EOT
     }
 }
