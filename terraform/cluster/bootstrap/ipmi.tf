a1a101b43d247099a4a1e8e7578071c6a7473c5b
Melvin Hillsman <mrhillsman@redhat.com>
2020-01-15 00:38:22 -0600

    Update ipmi.tf

--- terraform/cluster/bootstrap/ipmi.tf
+++ terraform/cluster/bootstrap/ipmi.tf
@@ -2,7 +2,7 @@ resource "null_resource" "ipmi_bootstrap" {
     provisioner "local-exec" {
         command = <<EOT
           ipmitool -I lanplus -H ${var.bootstrap_ipmi_host} -p ${var.bootstrap_ipmi_port} -U ${var.bootstrap_ipmi_user} -P ${var.bootstrap_ipmi_pass} chassis bootdev pxe;
-          ipmitool -I lanplus -H ${var.bootstrap_ipmi_host} -p ${var.bootstrap_ipmi_port} -U ${var.bootstrap_ipmi_user} -P ${var.bootstrap_ipmi_pass} power cycle || ipmitool -I lanplus -H ${var.bootstrap_ipmi_host} -p ${var.bootstrap_ipmi_port} -U ${var.bootstrap_ipmi_user} -P ${var.bootstrap_ipmi_pass} power on;
+          ipmitool -I lanplus -H ${var.bootstrap_ipmi_host} -p ${var.bootstrap_ipmi_port} -U ${var.bootstrap_ipmi_user} -P ${var.bootstrap_ipmi_pass} power reset || ipmitool -I lanplus -H ${var.bootstrap_ipmi_host} -p ${var.bootstrap_ipmi_port} -U ${var.bootstrap_ipmi_user} -P ${var.bootstrap_ipmi_pass} power on;
 EOT
     }
 }
