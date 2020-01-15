ca83dbc5643ba091ca1b158ff3682c166a134ddf
Melvin Hillsman <mrhillsman@redhat.com>
2020-01-15 00:38:07 -0600

    Update ipmi.tf

--- terraform/cluster/masters/ipmi.tf
+++ terraform/cluster/masters/ipmi.tf
@@ -3,7 +3,7 @@ resource "null_resource" "ipmi_master" {
     provisioner "local-exec" {
         command = <<EOT
           ipmitool -I lanplus -H ${var.master_nodes[count.index]["ipmi_host"]} -p ${var.master_nodes[count.index]["ipmi_port"]} -U ${var.master_nodes[count.index]["ipmi_user"]} -P ${var.master_nodes[count.index]["ipmi_pass"]} chassis bootdev pxe;
-          ipmitool -I lanplus -H ${var.master_nodes[count.index]["ipmi_host"]} -p ${var.master_nodes[count.index]["ipmi_port"]} -U ${var.master_nodes[count.index]["ipmi_user"]} -P ${var.master_nodes[count.index]["ipmi_pass"]} power cycle || ipmitool -I lanplus -H ${var.master_nodes[count.index]["ipmi_host"]} -p ${var.master_nodes[count.index]["ipmi_port"]} -U ${var.master_nodes[count.index]["ipmi_user"]} -P ${var.master_nodes[count.index]["ipmi_pass"]} power on;
+          ipmitool -I lanplus -H ${var.master_nodes[count.index]["ipmi_host"]} -p ${var.master_nodes[count.index]["ipmi_port"]} -U ${var.master_nodes[count.index]["ipmi_user"]} -P ${var.master_nodes[count.index]["ipmi_pass"]} power reset || ipmitool -I lanplus -H ${var.master_nodes[count.index]["ipmi_host"]} -p ${var.master_nodes[count.index]["ipmi_port"]} -U ${var.master_nodes[count.index]["ipmi_user"]} -P ${var.master_nodes[count.index]["ipmi_pass"]} power on;
 EOT
     }
 }
