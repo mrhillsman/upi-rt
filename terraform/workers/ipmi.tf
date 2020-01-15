d695326155445e9fa4736c1c7f5c43eef0386ec3
Melvin Hillsman <mrhillsman@redhat.com>
2020-01-15 00:37:41 -0600

    Update ipmi.tf

--- terraform/workers/ipmi.tf
+++ terraform/workers/ipmi.tf
@@ -3,7 +3,7 @@ resource "null_resource" "ipmi_worker" {
     provisioner "local-exec" {
         command = <<EOT
           ipmitool -I lanplus -H ${var.worker_nodes[count.index]["worker_ipmi_host"]} -p ${var.worker_nodes[count.index]["worker_ipmi_port"]} -U ${var.worker_nodes[count.index]["worker_ipmi_user"]} -P ${var.worker_nodes[count.index]["worker_ipmi_pass"]} chassis bootdev pxe;
-          ipmitool -I lanplus -H ${var.worker_nodes[count.index]["worker_ipmi_host"]} -p ${var.worker_nodes[count.index]["worker_ipmi_port"]} -U ${var.worker_nodes[count.index]["worker_ipmi_user"]} -P ${var.worker_nodes[count.index]["worker_ipmi_pass"]} power cycle || ipmitool -I lanplus -H ${var.worker_nodes[count.index]["worker_ipmi_host"]} -p ${var.worker_nodes[count.index]["worker_ipmi_port"]} -U ${var.worker_nodes[count.index]["worker_ipmi_user"]} -P ${var.worker_nodes[count.index]["worker_ipmi_pass"]} power on;
+          ipmitool -I lanplus -H ${var.worker_nodes[count.index]["worker_ipmi_host"]} -p ${var.worker_nodes[count.index]["worker_ipmi_port"]} -U ${var.worker_nodes[count.index]["worker_ipmi_user"]} -P ${var.worker_nodes[count.index]["worker_ipmi_pass"]} power reset || ipmitool -I lanplus -H ${var.worker_nodes[count.index]["worker_ipmi_host"]} -p ${var.worker_nodes[count.index]["worker_ipmi_port"]} -U ${var.worker_nodes[count.index]["worker_ipmi_user"]} -P ${var.worker_nodes[count.index]["worker_ipmi_pass"]} power on;
 EOT
     }
 }
