9c1fd30a2ef7d6259ff1f1325fcea4e718b021ce
Melvin Hillsman <mrhillsman@redhat.com>
2020-01-15 18:51:39 -0600

    Update ipmi.tf

--- terraform/workers/ipmi.tf
+++ terraform/workers/ipmi.tf
@@ -1,19 +1,19 @@
-resource "null_resource" "ipmi_worker" {
-    count = var.worker_count
-    provisioner "local-exec" {
-        command = <<EOT
-          ipmitool -I lanplus -H ${var.worker_nodes[count.index]["worker_ipmi_host"]} -p ${var.worker_nodes[count.index]["worker_ipmi_port"]} -U ${var.worker_nodes[count.index]["worker_ipmi_user"]} -P ${var.worker_nodes[count.index]["worker_ipmi_pass"]} chassis bootdev pxe;
-          ipmitool -I lanplus -H ${var.worker_nodes[count.index]["worker_ipmi_host"]} -p ${var.worker_nodes[count.index]["worker_ipmi_port"]} -U ${var.worker_nodes[count.index]["worker_ipmi_user"]} -P ${var.worker_nodes[count.index]["worker_ipmi_pass"]} power reset || ipmitool -I lanplus -H ${var.worker_nodes[count.index]["worker_ipmi_host"]} -p ${var.worker_nodes[count.index]["worker_ipmi_port"]} -U ${var.worker_nodes[count.index]["worker_ipmi_user"]} -P ${var.worker_nodes[count.index]["worker_ipmi_pass"]} power on;
-EOT
-    }
-}
-
-resource "null_resource" "ipmi_worker_cleanup" {
-    count = var.worker_count
-    provisioner "local-exec" {
-        when = "destroy"
-        command = <<EOT
-          ipmitool -I lanplus -H ${var.worker_nodes[count.index]["worker_ipmi_host"]} -p ${var.worker_nodes[count.index]["worker_ipmi_port"]} -U ${var.worker_nodes[count.index]["worker_ipmi_user"]} -P ${var.worker_nodes[count.index]["worker_ipmi_pass"]} power off;
-EOT
-    }
-}
+#resource "null_resource" "ipmi_worker" {
+#    count = var.worker_count
+#    provisioner "local-exec" {
+#        command = <<EOT
+#          ipmitool -I lanplus -H ${var.worker_nodes[count.index]["worker_ipmi_host"]} -p ${var.worker_nodes[count.index]["worker_ipmi_port"]} -U ${var.worker_nodes[count.index]["worker_ipmi_user"]} -P ${var.worker_nodes[count.index]["worker_ipmi_pass"]} chassis bootdev pxe;
+#          ipmitool -I lanplus -H ${var.worker_nodes[count.index]["worker_ipmi_host"]} -p ${var.worker_nodes[count.index]["worker_ipmi_port"]} -U ${var.worker_nodes[count.index]["worker_ipmi_user"]} -P ${var.worker_nodes[count.index]["worker_ipmi_pass"]} power reset || ipmitool -I lanplus -H ${var.worker_nodes[count.index]["worker_ipmi_host"]} -p ${var.worker_nodes[count.index]["worker_ipmi_port"]} -U ${var.worker_nodes[count.index]["worker_ipmi_user"]} -P ${var.worker_nodes[count.index]["worker_ipmi_pass"]} power on;
+#EOT
+#    }
+#}
+#
+#resource "null_resource" "ipmi_worker_cleanup" {
+#    count = var.worker_count
+#    provisioner "local-exec" {
+#        when = "destroy"
+#        command = <<EOT
+#          ipmitool -I lanplus -H ${var.worker_nodes[count.index]["worker_ipmi_host"]} -p ${var.worker_nodes[count.index]["worker_ipmi_port"]} -U ${var.worker_nodes[count.index]["worker_ipmi_user"]} -P ${var.worker_nodes[count.index]["worker_ipmi_pass"]} power off;
+#EOT
+#    }
+#}
